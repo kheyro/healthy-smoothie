4346b07981f25e34b5327a03faee5298edfa7fba
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-24 16:17:19 +0100

    added models relationships

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :smoothie_categories
+  has_many :smoothies, through: :smoothie_categories
 end
--- app/models/smoothie.rb
+++ app/models/smoothie.rb
@@ -1,3 +1,7 @@
 class Smoothie < ApplicationRecord
   belongs_to :user
+  has_many :smoothie_categories
+  has_many :categories, through: :smoothie_categories
+  has_many :quantities
+  has_many :ingredients, through: :quantities
 end
